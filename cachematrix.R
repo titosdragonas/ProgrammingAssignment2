cb16a656583ca87e1d269289ec4daf7fef461b7c
Titos Dragonas <titos.dragonas@nephos.mx>
2015-06-21 06:16:01 -0500

    Changed all mean references to inverse for readability

--- cachematrix.R
+++ cachematrix.R
@@ -10,11 +10,11 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -22,13 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getmean()
+  m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
   m <- solve(data)
-  x$setmean(m)
+  x$setinverse(m)
   m
 }
