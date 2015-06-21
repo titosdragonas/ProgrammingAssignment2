93cec9a178883b0d6247001284c3cf9a512f67e1
Titos Dragonas <titos.dragonas@nephos.mx>
2015-06-21 07:04:00 -0500

    Now with an abundance of comments!

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix returns a wrapper object (list of functions and their context) around the supplied matrix that is able to cache its inverse when using the cacheSolve function.
+## As per the assignment requirements it assumes that the supplied matrix is a square, invertible matrix so no checks or conversions are performed.
+## Once the list is created the user can: 
+## Use the get() and set() functions to read or replace the matrix.
+## Use the getinverse() and setinverse() functions to read or replace the cached inverse matrix.
+##
+## example use:
+## mymatrix<-makeCacheMatrix(matrix(rexp(16,rate=.1),ncol=4))
+## cacheSolve(mymatrix)
+## 
+## if mymatrix is invertible then mymatrix$get() %*% cacheSolve(mymatrix) must return an identity matrix:
+##
+## mymatrix$get()%*%cacheSolve(mymatrix)
+##              [,1]         [,2]          [,3]          [,4]
+## [1,]  1.000000e+00 2.368982e-17  3.068292e-17 -2.523481e-17
+## [2,] -1.046255e-16 1.000000e+00  5.410169e-17  1.016440e-17
+## [3,] -7.047314e-19 1.902775e-17  1.000000e+00  1.032703e-17
+## [4,] -2.439455e-17 8.977194e-17 -6.689527e-17  1.000000e+00
+## please note that the numbers outside the diagonal are almost zero - ten to the minus 16 or smaller
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  invc <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    invc <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
+  setinverse <- function(inverse) invc <<- inverse
+  getinverse <- function() invc
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns the inverse of a matrix that was created with the makeCacheMatrix function.
+## If the inverse is available in the cache it returns the cached copy.
+## If the inverse is not available in the cache it calculates, caches and then returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
+  tmp <- x$getinverse()
+  if(!is.null(tmp)) {
     message("getting cached data")
-    return(m)
+    return(tmp)
   }
   data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+  tmp <- solve(data)
+  x$setinverse(tmp)
+  tmp
 }
